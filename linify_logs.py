f25e65141f39d80a5f5bc4b710573d8f1040b016
Leif Lundgren <leiflundgren@leiflundgren.com>
2022-11-29 18:50:52 +0100

    better

--- linify_logs.py
+++ linify_logs.py
@@ -1,18 +1,20 @@
 
+from distutils.fancy_getopt import FancyGetopt
+from math import fabs
 import os
+from pickle import FALSE
 import re
 import sys
+from xmlrpc.client import boolean
 
 
 re_start_ms = '^\d\d\:\d\d\:\d\d\.\d\d\d\ \{\d+\} \[\d+\] \|\;\;'
 re_start_tas = '^\d\d\:\d\d\:\d\d\.\d\d\d\ \{\d+\} \[\d+\] \|[^|]*\|'
 
-re_end_ms = '\|.+\,\d+$'
-re_end_tas='\|\|.*\|\d+$'
 
-re_s = [ (re.compile(re_start_ms), re.compile(re_end_ms)), (re.compile(re_start_tas), re.compile(re_end_tas))]
+re_s = [ re.compile(re_start_ms), re.compile(re_start_tas) ]
 
-used_re = ()
+used_re = None
 
 if len(sys.argv) < 2:
     print("linify_logs inputfile.log [output.filename]")
@@ -28,21 +30,31 @@ else:
    
 print('output to ' + outfile)
 
+buf : str = None
 with open(infile, 'r') as in_f:
     with open(outfile, 'w') as out_f:
-
-
-
-
-        in_log = False
         count = 0
 
-        def write_line(l: str):
+
+        def write_line(l: str, in_log: boolean):
             if in_log:
                 out_f.write(l.rstrip())
                 out_f.write('\n')
             else:
                 out_f.write(l)
+
+        def add_buf(l: str):
+            global buf
+            if buf is None:
+                buf = l
+            else:
+                buf = buf.rstrip() + '\n' + l
+
+        def write_buf():
+            global buf
+            if buf:
+                write_line(buf, False)
+                buf = None
 
         while True:
             count += 1
@@ -51,31 +63,26 @@ with open(infile, 'r') as in_f:
             if not line: break;
 
             
-            if len(used_re) == 0:
-                for (re_start, re_end) in re_s:
+            if used_re is None:
+                for re_start in re_s:
                     if re_start.match(line) is not None:
-                        used_re = (re_start, re_end)
+                        used_re = re_start
                         break;
                 # if line doesn't match anything (like log-headers)
-                if len(used_re) == 0:
-                    write_line(line)
-                    in_log = False
+                if used_re is None:
+                    write_line(line, False)
                     continue;
 
-            match_end = used_re[1].match(line)
+            match_start = used_re.match(line)
 
-            if match_end is not None:
-                write_line(line)
-                in_log = False
-                continue;
+            if match_start:
+                write_buf()
 
-            match_start = used_re[0].match(line)
-
-            in_log = ( match_start is not None) # non-log line.
-            write_line(line)
+            add_buf(line)
 
 
-
+        # end of loop. write out cache
+        write_buf()
 
 
 
